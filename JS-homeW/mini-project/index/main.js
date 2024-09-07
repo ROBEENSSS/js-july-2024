3aab11362c6a60f947785d0a9aa2416ec4f8ecb9
ROBEENSSS <rmalhasan6@gmail.com>
2024-09-07 16:13:55 +0300

    complete 5 tasks mini-project

---
+++ JS-homeW/mini-project/index/main.js
@@ -0,0 +1,23 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        const containerUsers = document.getElementById("containerUsers");
+        if (!containerUsers) {
+            console.error('Куди дівся контейнер?');
+            return;
+        }
+        users.forEach(user => {
+            const userDiv = document.createElement('div');
+            userDiv.innerText = `ID: ${user.id} Name: ${user.name}`;
+
+            const button = document.createElement('button');
+            button.innerText = 'Details';
+            button.addEventListener('click', () => {
+                window.location.href = `../user-details/user-details.html?id=${user.id}`;
+            });
+            userDiv.appendChild(button);
+            containerUsers.appendChild(userDiv);
+        });
+    })
+    .catch(error => console.error('404 Сервак Шаліт', error));
+
---
+++ JS-homeW/mini-project/user-details/user-details.js
@@ -0,0 +1,67 @@
+function getQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    return {
+        id: params.get('id')
+    };
+}
+
+function objDetails(obj, container) {
+    const ul = document.createElement('ul');
+
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const li = document.createElement('li');
+
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                li.innerText = `${key}:`;
+                objDetails(obj[key], li);
+            } else {
+                li.innerText = `${key}: ${obj[key]}`;
+            }
+            ul.appendChild(li);
+        }
+    }
+    container.appendChild(ul);
+}
+
+const queryParams = getQueryParams();
+const userId = queryParams.id;
+
+if (userId) {
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then(response => response.json())
+        .then(user => {
+            const divUserDetails = document.getElementById('userDetails');
+            objDetails(user, divUserDetails);
+        })
+
+        .catch(error => {
+            console.error('404 Сервак Шаліт', error);
+            document.getElementById('userDetails').innerText = 'А де деталі юзера?';
+        });
+
+    const postsBtn = document.getElementById('postsBtn');
+    postsBtn.addEventListener('click', () => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+            .then(response => response.json())
+            .then(posts => {
+                const userPostsDiv = document.getElementById('userPosts');
+                const ul = document.createElement('ul');
+
+                posts.forEach(post => {
+                    const li = document.createElement('li');
+                    li.innerText = post.title;
+                    ul.appendChild(li);
+                });
+                userPostsDiv.appendChild(ul);
+
+                postsBtn.disabled = true;
+                postsBtn.innerText = 'Нє тикай, уже усьо загрузив))';
+            })
+
+            .catch(error => {
+                console.error('404 сервак шаліт с постами.', error);
+                document.getElementById('userPosts').innerText = 'Де його пости??';
+            });
+    })
+}
